105db85ed1678ee1ccd7d98311797d8bfc071533
SeoDongYeon1 <ehddus2655@gmail.com>
2023-05-26 15:19:51 +0900

    일정표 추가

---
+++ src/main/java/com/KoreaIT/sdy/demo/repository/ScheduleDao.java
@@ -0,0 +1,22 @@
+package com.KoreaIT.sdy.demo.repository;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.KoreaIT.sdy.demo.vo.DateData;
+import com.KoreaIT.sdy.demo.vo.ScheduleDto;
+
+@Mapper
+public interface ScheduleDao {
+
+	public int schedule_add(ScheduleDto scheduleDto);
+	public int before_schedule_add_search(ScheduleDto scheduleDto);
+	public ArrayList<ScheduleDto> schedule_list(DateData scheduleDto);
+	public ScheduleDto getSchedule(int idx);
+	public void deleteSchedule(int idx);
+	public void editSchedule(int schedule_idx, String schedule_subject, String schedule_desc, Date schedule_startdate, Date schedule_enddate);
+}
+
+
